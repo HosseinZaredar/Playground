c8b659a1035ae8547a8c15cf71948c3d71536f92
Hossein <hossein.zare9978@gmail.com>
2020-07-18 22:03:40 +0430

    functions

--- main.go
+++ main.go
@@ -4,33 +4,50 @@ import (
 	"fmt"
 )
 
+func sum(a int, b int) int {
+	return a + b
+}
+
+func swap(a, b int) (int, int) {
+	return b, a
+}
+
 func main() {
 
-	// variables
-	var v1 int = 1
-	var v2 int
-	v2 = 2
-	var v3 = 3
-	v4 := 4
+	/*
+		// variables
+		var v1 int = 1
+		var v2 int
+		v2 = 2
+		var v3 = 3
+		v4 := 4
 
-	fmt.Println(v1, v2, v3 ,v4)
+		fmt.Println(v1, v2, v3 ,v4)
 
-	// types
-	var a int = -3
-	var b int16 = 37
-	var c uint16 = 10
-	var d float32 = 3.14
-	var e bool = true
-	f := 3 + 2i // f := complex(3, 2)
-	str := "Hello!" // var str string = "Hello!"
+		// types
+		var a int = -3
+		var b int16 = 37
+		var c uint16 = 10
+		var d float32 = 3.14
+		var e bool = true
+		f := 3 + 2i // f := complex(3, 2)
+		str := "Hello!" // var str string = "Hello!"
 
-	fmt.Println(a, b, c, d, e, f, str)
+		fmt.Println(a, b, c, d, e, f, str)
 
-	// constants
-	const c1 = true
-	const c2 = 3.14
-	const c3 = -24
+		// constants
+		const c1 = true
+		const c2 = 3.14
+		const c3 = -24
 
-	fmt.Println(c1, c2, c3)
-	
+		fmt.Println(c1, c2, c3)
+	*/
+
+	a := 5
+	b := 6
+	fmt.Println(sum(a, b))
+
+	c, d := swap(a, b)
+	fmt.Println(c, d)
+
 }
