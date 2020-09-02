82d10e9c38ebfa060d1a977b83392ea2d6a8fef9
Hossein <hossein.zare9978@gmail.com>
2020-09-02 12:39:30 +0430

    Added comments for Rec function

--- WebGL2/lib.js
+++ WebGL2/lib.js
@@ -8,6 +8,7 @@ function Initialize(canvas) {
 
 function Rec(gl, {startX, startY, width, height}, {r, g, b}) {
 
+    // vertex shader
     var vertexShaderSource = `#version 300 es
  
     in vec2 a_position;
@@ -21,6 +22,7 @@ function Rec(gl, {startX, startY, width, height}, {r, g, b}) {
     }
     `;
     
+    // fragment shader
     var fragmentShaderSource = `#version 300 es
     
     precision highp float;
@@ -32,6 +34,7 @@ function Rec(gl, {startX, startY, width, height}, {r, g, b}) {
     }
     `;
 
+    // a function to compile a shader
     function createShader(gl, type, source) {
         var shader = gl.createShader(type);
         gl.shaderSource(shader, source);
@@ -43,6 +46,7 @@ function Rec(gl, {startX, startY, width, height}, {r, g, b}) {
         gl.deleteShader(shader);
     }
 
+    // a function to create a program from vertex and fragment shader
     function createProgram(gl, vertexShader, fragmentShader) {
         var program = gl.createProgram();
         gl.attachShader(program, vertexShader);
@@ -55,22 +59,22 @@ function Rec(gl, {startX, startY, width, height}, {r, g, b}) {
         gl.deleteProgram(program);
     }
 
+    // checking if Browser supports WebGL2
     if (gl === null) {
-        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        alert("Unable to initialize WebGL2. Your browser or machine may not support it.");
         return;
     }
 
+    // creating shaders
     var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
     var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
+    // creating the program
     var program = createProgram(gl, vertexShader, fragmentShader);
 
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
+    // creating vertx shader input buffer and filling it with proper data
     var positionBuffer = gl.createBuffer();
-
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
     var positions = [
         startX, startY,
         startX + width, startY,
@@ -81,28 +85,32 @@ function Rec(gl, {startX, startY, width, height}, {r, g, b}) {
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
+    // creating Vertex Array Object
     var vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
 
-    gl.enableVertexAttribArray(positionAttributeLocation);
-
+    // setting a_position using attribute
     var size = 2;
     var type = gl.FLOAT;
     var normalize = false;
     var stride = 0;
     var offset = 0;
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    gl.enableVertexAttribArray(positionAttributeLocation);
     gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
 
+    // setting the gl program
     gl.useProgram(program);
 
-    gl.bindVertexArray(vao);
-
+    // setting u_resolution in vertex shader
     var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
+    // setting u_color in fragment shader
     var colorLocation = gl.getUniformLocation(program, "u_color");
     gl.uniform4f(colorLocation, r / 256, g / 256, b / 256, 1);
 
+    // and finally, running out program
     var primitiveType = gl.TRIANGLES;
     var offset = 0;
     var count = 6;
