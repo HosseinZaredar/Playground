ae7e3f9ecd788edb60a26f8c19c49e4163de7efd
Hossein <hossein.zare9978@gmail.com>
2020-10-22 03:11:43 +0330

    Working on convolution animation

--- manim/scenes.py
+++ manim/scenes.py
@@ -4,20 +4,53 @@ from manimlib.imports import *
 
 class Plot(GraphScene):
     CONFIG = {
-        "y_max" : 6,
-        "y_min" : -6,
-        "x_max" : 9,
-        "x_min" : -9,
+        "y_max" : 9,
+        "y_min" : -9,
+        "x_max" : 18,
+        "x_min" : -18,
         "y_tick_frequency" : 1,
         "x_tick_frequency" : 1,
         "graph_origin" : ORIGIN,
         "y_axis_label": None,
         "x_axis_label": None,
+        "x_axis_width": 18,
+        "y_axis_height": 9,
+        # "y_labeled_nums": range(-9, 9, 1),
+        # "x_labeled_nums": range(-18, 18, 1)
     }
     def construct(self):
         self.setup_axes()
-        self.setup_signal([-1, 0, 1], [1, 2, 3])
+        x = np.array(range(-7, 7, 1))
+        y = 3 * np.sin(x)
+        
+        g1 = VGroup()
+        g2 = VGroup()
+
+
+        
+        # lines
+        for i in range(len(x)):
+            l1 = Line([x[i]/2, 0, 0], [x[i]/2, y[i]/2, 0])
+            l1.set_color(BLUE)
+            g1.add(l1)
+
+            l2 = Line([-x[i]/2, 0, 0], [-x[i]/2, y[i]/2, 0])
+            l2.set_color(BLUE)
+            g2.add(l2)
+
+        # dots
+        for i in range(len(x)):
+            d1 = Dot([x[i]/2, y[i]/2, 0])
+            g1.add(d1)
+            d2 = Dot([-x[i]/2, y[i]/2, 0])
+            g2.add(d2)
+
+        
+        self.play(Write(g1))
         self.wait(2)
+        self.play(Transform(g1, g2))
+        self.wait(2)
+
 
     def setup_axes(self):
         GraphScene.setup_axes(self)
@@ -38,12 +71,3 @@ class Plot(GraphScene):
             ],
             run_time=2
         )
-
-
-    def setup_signal(self, x, y):
-        for i in range(len(x)):
-            l = Line([x[i]/2, 0, 0], [x[i]/2, y[i]/2, 0])
-            l.set_color(BLUE)
-            self.play(ShowCreation(l), run_time=0.4)
-            d = Dot([x[i]/2, y[i]/2, 0])
-            self.play(Write(d), run_time=0.1)
